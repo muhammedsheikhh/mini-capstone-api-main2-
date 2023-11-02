8831da0dfc479b595c47c9c33a08ed60731724ba
“Muhammedssheikh” <mosheikh78690@gmail.com>
2023-11-02 14:34:43 -0500

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "mini-capstone.peterxjang.com", "localhost:8080", "stoic-payne-f85ae5.netlify.app"
+    origins "mini-capstone.peterxjang.com", "localhost:8080", "https://dazzling-sable-4325c6.netlify.app"
 
     resource "*",
       headers: :any,
